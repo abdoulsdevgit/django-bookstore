b148f4cc775c95672cb96d8d47a33ac35e90c26f
Abdoul Diallo <ad@ADs-MacBook-Air.local>
2020-10-02 20:53:10 -0400

    login logout and signup added

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.contrib.auth import login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
