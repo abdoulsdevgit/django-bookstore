8802c728c6cd7c69eaec150e7c27b6cbb0a2c618
Abdoul Diallo <ad@ADs-MacBook-Air.local>
2020-10-02 23:56:07 -0400

    added static files and crispy forms

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party 
+    'crispy_forms',
+
     # local apps
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -126,7 +129,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# static directories settings
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder", 
+]
+
 AUTH_USER_MODEL = "users.CustomUser"
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pages/tests.py
+++ pages/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse, resolve
 
 
 # local imports
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomePageTest(SimpleTestCase):
@@ -16,10 +16,10 @@ class HomePageTest(SimpleTestCase):
     def test_homepage_status_code(self) -> None: 
         self.assertEqual(self.response.status_code, 200)
 
-    def test_homepage_template(self):
+    def test_homepage_template(self) -> None:
         self.assertTemplateUsed(self.response, 'home.html')
 
-    def test_homepage_contains_correct_html(self):
+    def test_homepage_contains_correct_html(self) -> None:
         self.assertContains(self.response, 'Homepage')
 
 
@@ -31,3 +31,26 @@ class HomePageTest(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve('/')
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+class AboutPageTest(TestCase):
+
+    def setUp(self) -> None:
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_exist(self) -> None:
+        self.assertAlmostEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self) -> None:
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self) -> None: 
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self) -> None: 
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
+
+    def test_aboutpage_url_resolves_aboutpageview(self) -> None: 
+        view = resolve('/about/')
+        self.assertEqual(view.func.__name__,
+            AboutPageView.as_view().__name__)
